92edbbccf8064bcf5e67b251d9e01578f5fcc650
Dan Moneypenny <catullus1973@gmail.com>
2022-04-17 07:10:04 -0700

    update

---
+++ app/public/bundle.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+const initForms = ($message) => {
+  console.log($message);
+};
+
+//import './modules/bootstrap.bundle';
+initForms('Init forms is executed');
+
+}());
+//# sourceMappingURL=bundle.js.map
